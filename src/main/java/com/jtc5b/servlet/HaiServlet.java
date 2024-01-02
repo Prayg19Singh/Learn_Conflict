f49f315ba00da9fe66be2cfc5bc805acd788378d
Prayag <prayagsingh19bbau@gmail.com>
2024-01-02 10:07:26 +0530

    first commit

---
+++ src/main/java/com/jtc5b/servlet/HaiServlet.java
@@ -0,0 +1,23 @@
+package com.jtc5b.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name="any_random-name", urlPatterns="/form_action", 
+loadOnStartup=2, 
+initParams= {@WebInitParam(name="email2", value="Hai@gmail.com")})
+public class HaiServlet extends HttpServlet {
+	public void init(ServletConfig sc) {
+		System.out.println("init method in HaiServlet");
+	}
+	public void service(ServletRequest req, ServletResponse res) {
+		System.out.println("service method in HaiServlet");
+	}
+	public void destroy() {
+		System.out.println("destroy method in HaiServlet");
+	}
+}
---
+++ src/main/java/com/jtc5b/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.jtc5b.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name="random_name", urlPatterns="/form.action", loadOnStartup=1,
+initParams= {@WebInitParam(name="email1", value="Hello@gmail.com")})
+public class HelloServlet extends HttpServlet{
+	public void init(ServletConfig sc) {
+		System.out.println("init method in HelloServlet");
+	}
+	public void service(ServletRequest req, ServletResponse res) {
+		System.out.println("service metod in HelloServlet");
+	}
+	public void destroy() {
+		System.out.println("destroy method in HelloServlet");
+	}
+}
