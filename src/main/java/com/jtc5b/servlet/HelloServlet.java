af97a2ee0fe15938139b04e8b823e0bc78ce6aca
Prayag <prayagsingh19bbau@gmail.com>
2024-01-03 16:30:02 +0530

    learn again pull request

--- src/main/java/com/jtc5b/servlet/HelloServlet.java
+++ src/main/java/com/jtc5b/servlet/HelloServlet.java
@@ -22,4 +22,5 @@ public class HelloServlet extends HttpServlet{
 	
 	lets make some changes to understand the branch concept.
 	lets understand the concept of pull request!!!
+	lets do some changes again to better understand the pull request!! 
 }
